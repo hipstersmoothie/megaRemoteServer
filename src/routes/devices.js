724c3d86eada6d0278d25eaeb16b1990ef3dcfd8
Andrew <lisowski54@gmail.com>
2017-02-02 00:59:24 -0800

    switch route

--- src/routes/devices.js
+++ src/routes/devices.js
@@ -14,21 +14,45 @@ router.get('/devices/:device', (req, res) => {
   getResource('devices', res, hUtil => hUtil._readDeviceCommands(req.params.device));
 });
 
-router.post('/devices/:device/:command', (req, res) => {
-  if (req.params.device === 'Vizio TV' && req.params.command === 'PowerOn') {
-    plug.getConsumption().then((consumption) => {
-      if (consumption.get_realtime.power < 50) {
-        doCommand('PowerToggle');
-      } else {
-        res.send(false);
-      }
-    });
-  } else {
-    doCommand();
+let main = '';
+let secondary = '';
+
+function setAreas(req) {
+  if (req.params.device === 'Onkyo AV Receiver' && req.params.command !== 'PowerOn' && req.params.command !== 'PowerOff') {
+    main = req.params.command;
   }
 
-  function doCommand(command) {
-    getResource('devices', res, hUtil => hUtil.executeCommand(true, req.params.device, command || req.params.command));
+  if (req.params.device === 'Onkyo AV Receiver (2)' && req.params.command !== 'PowerOn' && req.params.command !== 'PowerOff') {
+    secondary = req.params.command;
+  }
+}
+
+function doCommand(req, res, device, command) {
+  getResource('devices', res, hUtil => hUtil.executeCommand(true, device || req.params.device, command || req.params.command));
+}
+
+function VizioOn(req, res) {
+  plug.getConsumption().then((consumption) => {
+    if (consumption.get_realtime.power < 50) {
+      doCommand(req, res, null, 'PowerToggle');
+    } else {
+      res.send(false);
+    }
+  });
+}
+
+router.post('/switch', (req, res) => {
+  doCommand(req, { send: () => {} }, 'Onkyo AV Receiver', secondary);
+  doCommand(req, res, 'Onkyo AV Receiver (2)', main);
+});
+
+router.post('/devices/:device/:command', (req, res) => {
+  setAreas(req);
+
+  if (req.params.device === 'Vizio TV' && req.params.command === 'PowerOn') {
+    VizioOn(req, res);
+  } else {
+    doCommand(req, res);
   }
 });
 
